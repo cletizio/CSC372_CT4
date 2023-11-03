8d25b917ec7254d0a85c37ff89e00b666529d7ac
Chris Letizio <cletizio@gmail.com>
2023-11-03 18:02:45 -0400

    Added and got Cone working

--- src/Cone.java
+++ src/Cone.java
@@ -1,12 +1,24 @@
 public class Cone extends Shape{
+    private double radius = 0.0;
+    private double height = 0.0;
 
-    @Override
-    public double surfaceArea() {
-        return 0;
+    public Cone(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
     }
 
     @Override
-    public double volume() {
-        return 0;
+    public double surfaceArea () {
+        double sideLength = Math.sqrt((height * height) + (radius * radius));
+        return Math.PI * radius * (radius + sideLength);
+    }
+
+    @Override
+    public double volume () {
+        return (1.0 / 3.0) * Math.PI * Math.pow(radius, 2) * height;
+    }
+
+    public String toString () {
+        return ("Surface area of Cone: " + surfaceArea() + ", Volume of Cone: " + volume());
     }
 }
--- src/ShapeArray.java
+++ src/ShapeArray.java
@@ -1,8 +1,9 @@
 public class ShapeArray {
     public static void main(String[] args){
         Sphere sphere = new Sphere(4.0);
+        Cone cone = new Cone( 5.0, 8.0);
 
-        Shape[] shapeArray = { sphere};
+        Shape[] shapeArray = { sphere, cone};
 
         for (Shape shape : shapeArray){
             System.out.println(shape.toString());
