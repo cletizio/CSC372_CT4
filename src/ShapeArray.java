7dbd011c7a25722195f3145b977792ac1d7e0fb4
Chris Letizio <cletizio@gmail.com>
2023-11-03 18:38:33 -0400

    Added and got Cylinder working

--- src/ShapeArray.java
+++ src/ShapeArray.java
@@ -1,12 +1,12 @@
 public class ShapeArray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Sphere sphere = new Sphere(4.0);
-        Cone cone = new Cone( 5.0, 8.0);
-        Cylinder cylinder = new Cylinder(6.0,9.0);
+        Cone cone = new Cone(5.0, 8.0);
+        Cylinder cylinder = new Cylinder(6.0, 9.0);
 
         Shape[] shapeArray = {sphere, cone, cylinder};
 
-        for (Shape shape : shapeArray){
+        for (Shape shape : shapeArray) {
             System.out.println(shape.toString());
         }
     }
